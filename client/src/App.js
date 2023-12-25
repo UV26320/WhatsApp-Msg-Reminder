738997fa2180a37649aac3c8e1127622d549f31e
UDAY VARA <udayvara26@gmail.com>
2023-12-25 22:42:20 +0530

    App.js updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,42 @@
-import './App.css';
-import React,{ useState,useEffect } from 'react';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import DateTimePicker from 'react-datetime-picker';
+import DateTimePicker from "react-datetime-picker";
 
 function App() {
+  const [reminderMsg, setReminderMsg] = useState("");
+  const [remindAt, setRemindAt] = useState("");
 
-const [ reminderMsg, setReminderMsg] = useState('');
-const [ remindAt , setRemindAt ] = useState('');
+  const addReminder = () => {
+    
+  }
 
   return (
     <div className="App">
-       <div className='homepage'>
-        <div className='homepage_Header'>
-          <h1>
-            Remind Me 🙋🏻‍♂️
-          </h1>
-          <input type='text' placeholder='Reminder Text here....' value={reminderMsg} onChange={e => setReminderMsg} />
-          <DateTimePicker 
+      <div className="homepage">
+        <div className="homepage_Header">
+          <h1>Remind Me 🙋🏻‍♂️</h1>
+          <input
+            type="text"
+            placeholder="Reminder Text here...."
+            value={reminderMsg}
+            onChange={(e) => setReminderMsg}
+          />
+          <DateTimePicker
             value={remindAt}
             onChange={setRemindAt}
             minDate={new Date()}
-            minutePlaceholder='mm'
-            hourPlaceholder='hh'
-            dayPlaceholder='DD'
-            monthPlaceholder='MM'
-            yearPlaceholder='YYYY'
+            minutePlaceholder="mm"
+            hourPlaceholder="hh"
+            dayPlaceholder="DD"
+            monthPlaceholder="MM"
+            yearPlaceholder="YYYY"
           />
         </div>
-       </div>
+        <div className="button" onClick={addReminder}>
+          Add Reminder
+        </div>
+      </div>
     </div>
   );
 }

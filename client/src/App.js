889c004a24ec42fb34c3b63d0bb81f56d9abd2f7
UDAY VARA <udayvara26@gmail.com>
2023-12-25 22:21:11 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import React,{ useState,useEffect } from 'react';
+import axios from "axios";
+import DateTimePicker from 'react-datetime-picker';
+
+function App() {
+
+const [ reminderMsg, setReminderMsg] = useState('');
+const [ remindAt , setRemindAt ] = useState('');
+
+  return (
+    <div className="App">
+       <div className='homepage'>
+        <div className='homepage_Header'>
+          <h1>
+            Remind Me 🙋🏻‍♂️
+          </h1>
+          <input type='text' placeholder='Reminder Text here....' value={reminderMsg} onChange={e => setReminderMsg} />
+          <DateTimePicker 
+            value={remindAt}
+            onChange={setRemindAt}
+            minDate={new Date()}
+            minutePlaceholder='mm'
+            hourPlaceholder='hh'
+            dayPlaceholder='DD'
+            monthPlaceholder='MM'
+            yearPlaceholder='YYYY'
+          />
+        </div>
+       </div>
+    </div>
+  );
+}
+
+export default App;

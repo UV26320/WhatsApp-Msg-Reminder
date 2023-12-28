7e0558be32e9dfc7eb11c60cf5030d4783bb4ede
UDAY VARA <udayvara26@gmail.com>
2023-12-28 19:44:02 +0530

    App.js updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import DataPicker from "./DataPicker";
 
 function App() {
   const [reminderMsg, setReminderMsg] = useState("");
+  const [remindAt, setRemindAt] = useState(getDefaultDateTime());
   const [reminderList, setReminderList] = useState([]);
 
   useEffect(() => {
@@ -23,18 +23,36 @@ function App() {
 
   const deleteReminder = () => {};
 
+  function getDefaultDateTime() {
+    // Set a default time (e.g., 12:00 PM)
+    const defaultTime = "12:00";
+    // Get current date in YYYY-MM-DD format
+    const today = new Date().toISOString().split("T")[0];
+    // Combine default time with current date to match the 'datetime-local' format
+    return `${today}T${defaultTime}`;
+  }
+
   return (
     <div className="App">
       <div className="homepage">
         <div className="homepage_Header">
           <h1>Remind Me 🙋🏻‍♂️</h1>
+
           <input
             type="text"
             placeholder="Reminder Text here...."
             value={reminderMsg}
             onChange={(e) => setReminderMsg(e.target.value)}
           />
-          <DataPicker></DataPicker>
+          <div>
+            <input
+              className="datePicker"
+              type="datetime-local"
+              onChange={(e) => setRemindAt(e.target.value)}
+            />
+            <p>Default Selected Date: {remindAt}</p>
+          </div>
+
           <div className="button" onClick={addReminder}>
             Add Reminder
           </div>

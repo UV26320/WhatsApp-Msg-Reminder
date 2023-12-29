340f2b03990d11155a95688471a3bef4d06ee218
UDAY VARA <udayvara26@gmail.com>
2023-12-29 19:36:59 +0530

    mongodb atlas connection establish

--- server/config/dbconnection.js
+++ server/config/dbconnection.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnect = async() => {
     try{
-        //  const conn = await mongoose.connect(process.env.MONGO_URL);
-         const conn = await mongoose.connect('mongodb://127.0.0.1:27017/reminderAppDB')
+         const conn = await mongoose.connect(process.env.MONGO_URL);
          console.log(`Database Connected To Host ${conn.connection.host}`);
     }
     catch(error){
